e42a81d33177d675c3cc357a0ed7f704a9a2784b
Dominik Lisovski <Dominik.Lisovski@gmail.com>
2019-01-20 20:44:47 +0200

    fixed brackets

--- BiosensorSimulator/Calculators/SchemeCalculator/ImplicitSchemeCalculator.cs
+++ BiosensorSimulator/Calculators/SchemeCalculator/ImplicitSchemeCalculator.cs
@@ -55,7 +55,7 @@ namespace BiosensorSimulator.Calculators.SchemeCalculator
             for (var i = 1; i < layer.N - 1; i++)
             {
                 var fermentReactionSpeed = Biosensor.VMax * sPrev[i] / (Biosensor.Km + sPrev[i]);
-                f[i] = -(sPrev[i] + -SimulationParameters.t * fermentReactionSpeed);
+                f[i] = -sPrev[i] + SimulationParameters.t * fermentReactionSpeed;
             }
             
             CalculateNextStep(layer, layer.Substrate, sCur, f);
@@ -63,7 +63,7 @@ namespace BiosensorSimulator.Calculators.SchemeCalculator
             for (var i = 1; i < layer.N - 1; i++)
             {
                 var fermentReactionSpeed = Biosensor.VMax * sPrev[i] / (Biosensor.Km + sPrev[i]);
-                f[i] = -(pPrev[i] + SimulationParameters.t * fermentReactionSpeed);
+                f[i] = -pPrev[i] - SimulationParameters.t * fermentReactionSpeed;
             }
 
             CalculateNextStep(layer, layer.Product, pCur, f);
